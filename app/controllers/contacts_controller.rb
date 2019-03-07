06de500041b92702fb24578b2210e17b9e77194a
skmahato <souravkumar1329@gmail.com>
2019-03-07 23:55:06 +0530

    api index create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,19 @@ class ContactsController < ApplicationController
     contacts = Contact.all
     render json: contacts
   end
+
+  def create
+    contact = Contact.new(contact_params)
+    if contact.save
+      render_success(:ok, contact)
+    else
+      render_error(:unprocessable_entity, contact.errors)
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :number)
+  end
 end
