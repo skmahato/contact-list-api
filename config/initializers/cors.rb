d6906031f1bc80701a49fd9cc5160e5d72d9c407
skmahato <souravkumar1329@gmail.com>
2019-04-07 12:14:35 +0530

    modified cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,9 +13,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-end
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://reactjs-contact-list.herokuapp.com/'
 
