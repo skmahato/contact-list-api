f623e89141eae1a7d19aa3eb8c24d0cd82778855
skmahato <souravkumar1329@gmail.com>
2019-04-07 20:07:09 +0530

    removed require

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -34,6 +34,6 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :number)
+    params.permit(:name, :number)
   end
 end
